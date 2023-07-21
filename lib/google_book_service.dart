7106e797abeed61071cd3dadea54f6ffa6e4c0f5
ikyrie <mah.c5r@gmail.com>
2023-07-21 11:06:34 -0300

    Ajustando o arquivo de serviços

--- lib/google_book_service.dart
+++ lib/google_book_service.dart
@@ -22,3 +22,45 @@ class GoogleBooksService {
     return List.empty();
   }
 }
+
+class GoogleBook {
+  late String id;
+  late String title;
+  late String authors;
+  late String description;
+  late String thumbnailLink;
+
+  GoogleBook({
+    required this.id,
+    required this.title,
+    required this.authors,
+    required this.description,
+    required this.thumbnailLink,
+  });
+
+  GoogleBook.fromApi(Map<String, dynamic> map) {
+    id = map["id"];
+    title = map["volumeInfo"]["title"];
+    authors = (map["volumeInfo"]["authors"] as List<dynamic>).map((e) => e).toString();
+    description = map["volumeInfo"]["description"];
+    thumbnailLink = map["volumeInfo"]["imageLinks"]?["thumbnail"];
+  }
+
+  GoogleBook.fromJson(Map<String, dynamic> map) {
+    id = map["id"];
+    title = map["title"];
+    authors = map["authors"];
+    description = map["description"];
+    thumbnailLink = map["thumbnailLink"];
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      "id": id,
+      "title": title,
+      "authors": authors,
+      "description": description,
+      "thumbnailLink": thumbnailLink,
+    };
+  }
+}
